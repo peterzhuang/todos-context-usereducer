6123b8196b37dc8cbaefb4a0328782c4dc9a37fa
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2019-11-07 17:26:57 -0500

    L279 start consuming Todos Context

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -7,6 +7,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 import useTodoState from "./hooks/useTodoState";
+import { TodosProvider } from "./context/todos.context";
 
 function TodoApp() {
   const initialTodos = [{ id: 1, task: "Walk The Goldfish", completed: true }];
@@ -30,13 +31,15 @@ function TodoApp() {
       </AppBar>
       <Grid container justify='center' style={{ marginTop: "1rem" }}>
         <Grid item xs={11} md={8} lg={4}>
-          <TodoForm addTodo={addTodo} />
-          <TodoList
-            todos={todos}
-            removeTodo={removeTodo}
-            toggleTodo={toggleTodo}
-            editTodo={editTodo}
-          />
+        <TodosProvider>
+            <TodoForm addTodo={addTodo} />
+            <TodoList
+              todos={todos}
+              removeTodo={removeTodo}
+              toggleTodo={toggleTodo}
+              editTodo={editTodo}
+            />
+          </TodosProvider>
         </Grid>
       </Grid>
     </Paper>
